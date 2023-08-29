235c6113aa290eab5ed0295d90e9b8e9b5b144b4
2karina <karina_kap@hotmail.com>
2023-08-29 18:16:21 -0300

    T001-KARINA: feat - html of login.js

--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import login from './pages/login/login.js'
 import register from './pages/login/register.js'
 import timeline from './pages/timeline/timeline.js'
 
+const main = document.querySelector("#root");
 
+window.addEventListener("load", () => {
+main.appendChild(login());
+})
