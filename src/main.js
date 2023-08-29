3d81c3cd043f41e02f61e1735dec8c1ab09eb070
Gabriela Faria <gabrielafaria806@gmail.com>
2023-08-29 17:10:21 -0300

    fix - change in folders structure and creation of css files

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import login from './pages/login/login.js'
-import register from './pages/register/register.js'
+import register from './pages/login/register.js'
 import timeline from './pages/timeline/timeline.js'
 
 
