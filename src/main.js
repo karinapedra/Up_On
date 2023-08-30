d34c27be103de136a120597181b0506564d1d904
Gabriela Faria <gabrielafaria806@gmail.com>
2023-08-29 21:14:05 -0300

    T002-GABI: style - changes in the login.css

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import login from './pages/login/login.js'
 import register from './pages/login/register.js'
-import timeline from './pages/timeline/timeline.js'
+//import timeline from './pages/timeline/timeline.js'
 
 const main = document.querySelector("#root");
 
 window.addEventListener("load", () => {
-main.appendChild(login());
+    main.appendChild(login());
 })
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,19 +1,21 @@
 export default () => {
-    const container = document.createElement('div');
-    container.classList.add('formContent')
-    const template = `
-    <h2 class="formTitle">Sign in</h2>
-    <p class="formTitle"> Stay up on the latest happenings from around the world, right here and now.</p>
+  const container = document.createElement("div");
+  container.classList.add("formContent");
+  const template = `
+    <section class="formTitle">
+        <h2>Sign in</h2>
+        <p> Stay up on the latest happenings from around the world, right here and now.</p>
+    </section>
     <input type="email" class="formInput" placeholder="E-mail"></input>
     <input type="password" class="formInput" placeholder="Password"></input>
+    <a href="#" class="smallThings">Forgot your password?</a>
     <button class="formButtons">Sign In</button>
-    <a href="#" class="forgotPassoword">Forgot your password?</a>
-    <p>____________________ or ____________________</p>
+    <p>_____________ or ____________</p>
     <button class="formButtons" id="google">Continue with Google</button>
-    <p>New in Up_On? <a href="#" class="signUp">SignUp</a>
-    <p>About<a href="#" class="about">About</a>
-`
+    <p class="smallThings">New in Up_On? <a href="#" >SignUp</a></p>
+    <a href="#" class="smallThings">Learn About Up_On</a>
+`;
 
-    container.innerHTML = template; 
-    return container;
-}
+  container.innerHTML = template;
+  return container;
+};
