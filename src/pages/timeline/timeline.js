4e791b97fbb61fa92110bc04325430cb96fc1e84
erikapeloggia <erikapeloggia@gmail.com>
2023-09-20 15:19:18 -0300

    T013-ERIKA: feat integration of edit and delete functions in the timeline

--- src/pages/timeline/timeline.js
+++ src/pages/timeline/timeline.js
@@ -5,6 +5,8 @@ import {
   calculateTimeAgo,
   getPosts,
   votePost,
+  editPost,
+  deletePost
 } from "../../firebase/firebase.js";
 
 export default () => {
@@ -13,107 +15,60 @@ export default () => {
 
   const template = `
     <header class="header-timeline">
-      <nav>
+      <nav class="menu">
         <ul class="headerMenu-timeline">
-          <li>
-            <img src="" id="userImageHeader" alt="profileImage">
+          <li><img src="" id="userImageHeader" alt="profileImage"></li>
           <li>
             <a href="#timeline" id="logo">
               <img src="assets/up_on_logo.png" alt="up_on_logo" />
             </a>
           </li>
           <li>
-            <a href="#search">
-              <img src="assets/search.png" alt="search" />
-            </a>
+            <button class="signOut">
+              <img class="signOutIcon" src="./assets/signOut.png" alt="Sign Out">
+            </button>
           </li>
         </ul>
     </nav>
     </header>
     <section class="allPosts"></section>
-    <div id="fade-createPost" class="hide"></div>
-    <div id="modal-createPost" class="hide">
-      <div class="modal-header">
-        <img src="" id="userImage" class="imgProfile" alt="Profile Image"/>
-        <p id="userName"></p>
-      </div>
-      <div class="modal-body">
-        <textarea id="createPost" placeholder="What's happening?" maxlength="150"></textarea>
-        <section class="buttonsCreate"> 
-          <button class="postButtonsCreate" id="close-modal-createPost">CANCEL</button>
-          <button class="postButtonsCreate" id="sendPost">POST</button>
-        </section>
-      </div>
-    </div>
-
     <button class="createButton" id="open-modal-createPost"><img src="./assets/create.png"></button>
-    <button id="signOut">Sign Out</button>
     `;
 
-  // <div id="fade-deletePost" class="hide"></div>
-  // <div id="modal-deletePost" class="hide">
-  //   <div class="modal-header">
-  //     <img src="" id="userImage" class="imgProfile" alt="Profile Image"/>
-  //     <p id="userName"></p>
-  //   </div>
-  //   <div class="modal-body">
-  //     <textarea id="createPost" placeholder="What's happening?" maxlength="150"></textarea>
-  //     <section class="buttonsCreate">
-  //       <button class="postButtonsCreate" id="close-modal-deletePost">CANCEL</button>
-  //       <button class="postButtonsCreate" id="sendPost">POST</button>
-  //     </section>
-  //   </div>
-  // </div>
 
   container.innerHTML = template;
 
-  const signOutButton = container.querySelector("#signOut");
-  const userImage = container.querySelector("#userImage");
-  const userName = container.querySelector("#userName");
-  const header = container.querySelector(".header");
-  const footer = container.querySelector(".footer");
+  const signOutButton = container.querySelector(".signOut");
   const userImageHeader = container.querySelector("#userImageHeader");
-  const openModalCreatePostButton = container.querySelector("#open-modal-createPost");
-  const closeModalCreatePostButton = container.querySelector("#close-modal-createPost");
-  const modalCreatePost = container.querySelector("#modal-createPost");
-  const fadeCreatePost = container.querySelector("#fade-createPost");
   const allPosts = container.querySelector(".allPosts");
-  const sendPostButton = container.querySelector("#sendPost");
-  const createPost = container.querySelector("#createPost");
-  const closeSendPost = container.querySelector("#sendPost");
-
+  const openModalCreate = container.querySelector(".createButton");
+ 
   const user = getUserInfo();
-  userImage.src = user.photoURL;
-  userName.innerHTML = user.displayName;
   userImageHeader.src = user.photoURL;
 
 
-  const toggleModal = () => {
-    modalCreatePost.classList.toggle("hide");
-    fadeCreatePost.classList.toggle("hide");
-  };
-
-  [
-    openModalCreatePostButton,
-    closeModalCreatePostButton,
-    fadeCreatePost,
-    closeSendPost,
-  ].forEach((el) => {
-    el.addEventListener("click", () => toggleModal());
-  });
-
-  sendPostButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    if (createPost.value !== "") {
-      addPost(createPost.value, user.displayName, user.photoURL, user.uid);
-      allPosts.innerHTML = ""
-      loadPosts()
-    }
-  });
+  openModalCreate.addEventListener("click", () =>{
+    modalCreate.modal.classList.remove("hide");
+    modalCreate.confirmButton.addEventListener("click", (event) => {
+      event.preventDefault();
+      if (modalCreate.inputModal.value !== "") {
+        addPost(modalCreate.inputModal.value, user.displayName, user.photoURL, user.uid);
+        modalCreate.inputModal.value = "";
+        modalCreate.modal.classList.add("hide");
+      }
+    });
+  })
 
   const createPostElement = (post) => {
-    const postFeed = document.createElement("article");
-    postFeed.classList.add("posts");
+    const articleId = `article-${post.docRef}`;
+    let postFeed = document.getElementById(articleId);
+    if (postFeed) {
+      postFeed.innerHTML = "";
+    } else {
+      postFeed = document.createElement("article");
+      postFeed.id = articleId;
+      postFeed.classList.add("posts");
+    }
     const template = `
       <section class="postHeader">
         <section class="postUserInfo">
@@ -122,19 +77,19 @@ export default () => {
           <p class="timestamp">${calculateTimeAgo(post.data.toDate())}</p>
         </section>  
         ${post.userUID === user.uid ? `
-        <button class="postButtons"><img src="./assets/edit.png"></button>
+        <button class="postButtons openModalEdit" data-content="${post.content}" data-id="${post.docRef}"><img src="./assets/edit.png"></button>
         ` : ""}
       </section>
       <p class="content">${post.content}</p>
       <section class="postFooter">
         <section class="votes">
-          <button class="upVoteButton" id="${
+          <button class="upVoteButton" data-id="${
             post.docRef
           }"><img src="./assets/upvote.png"></button>
           <p class="countVotes">${post.votes.length}</p>
         </section>
         ${post.userUID === user.uid ? `
-        <button class="postButtons"><img src="./assets/remove.png"></button>
+        <button class="postButtons openModalDelete" data-id="${post.docRef}"><img src="./assets/remove.png"></button>
         ` : ""}
       </section>
     `;
@@ -144,22 +99,123 @@ export default () => {
 
   const votePosts = (docRef) => {
       votePost(docRef, user.uid);
-      allPosts.innerHTML = ""
   };
 
-  const createAllPosts = (posts) => {
-    createPostElement(posts);
-  };
+  const modalCreateEdit = (isNewPost) =>{
+    const modalContent = `
+    <div class="fade-createPost hide"></div>
+    <div class="modal-createPost hide">
+      <div class="modal-header">
+        <img src="" class="userImage imgProfile" alt="Profile Image"/>
+        <p class="userName"></p>
+      </div>
+      <div class="modal-body">
+        <textarea class="createPost" placeholder="What's happening?" maxlength="150"></textarea>
+        <section class="buttonsCreate"> 
+          <button class="postButtonsCreate cancelButton close-modal-createPost" >CANCEL</button>
+          ${isNewPost ? `<button class="postButtonsCreate confirmButton sendPost">POST</button>` 
+          : `<button class="postButtonsCreate confirmButton sendEditPost">EDIT</button>`}
+        </section>
+      </div>
+    </div>
+    `
+    const modalContainer = document.createElement('article');
+    modalContainer.innerHTML = modalContent;
+  
+    document.body.appendChild(modalContainer);
+    
+    const modal = modalContainer.querySelector('.modal-createPost');
+    const fade = modalContainer.querySelector('.fade-createPost');
+    const cancelButton = modalContainer.querySelector('.close-modal-createPost');
+    const confirmButton = modalContainer.querySelector('.sendPost');
+    const confirmEditButton = modalContainer.querySelector(".sendEditPost");
+    const userImage = modalContainer.querySelector(".userImage");
+    const userName = modalContainer.querySelector(".userName");
+    const inputModal = modalContainer.querySelector(".createPost");
+
+    userImage.src = user.photoURL;
+    userName.innerHTML = user.displayName;
+
+    cancelButton.addEventListener('click', () => {
+      modal.classList.add('hide');
+      fade.classList.add('hide');
+    });
+
+    if (confirmButton) {
+      return { modal, fade, confirmButton, inputModal };
+    } else {
+      return { modal, fade, confirmEditButton, inputModal };
+    }
+  }
+
+  const modalCreate = modalCreateEdit(true);
+  const modalEdit = modalCreateEdit(false);
+
+  const createModalDelete = () => {
+    const modalContent =  `
+      <div id="fade-delete" class="hide"></div>
+      <div id="modal-delete" class="modalDelete hide">
+      <section class="modalContent">  
+        <section class="modal-header-delete">
+          <h3>DELETE</h3>
+        </section>
+        <section class="modal-body-delete">
+          <p>Are you sure? </p>
+          <p>Your post will be permanently deleted.</p>
+          <section class="buttons-delete">
+            <button class="postButtonsCreate closeModalDelete cancelButton">CANCEL</button>
+            <button class="postButtonsCreate confirmDelete confirmButton">DELETE</button>
+          </section>
+        </section>
+      </section>
+      </div>
+    `;
+
+    const modalContainer = document.createElement('article');
+    modalContainer.innerHTML = modalContent;
+  
+    document.body.appendChild(modalContainer);
+  
+    const modalDelete = modalContainer.querySelector('.modalDelete');
+    const fadeDelete = modalContainer.querySelector('#fade-delete');
+    const cancelButton = modalContainer.querySelector('.closeModalDelete');
+    const confirmButton = modalContainer.querySelector('.confirmDelete');
+  
+    cancelButton.addEventListener('click', () => {
+      modalDelete.classList.add('hide');
+      fadeDelete.classList.add('hide');
+    });
+  
+    return { modalDelete, fadeDelete, confirmButton };
+  }
+
+  const { modalDelete, fadeDelete, confirmButton } = createModalDelete();
 
   const loadPosts = async () => {
-    getPosts(createAllPosts);
+    getPosts(createPostElement);
   };
 
   container.addEventListener("click", (event) => {
     const targetParent = event.target.parentNode;
     if(targetParent.classList.contains("upVoteButton")) {
-      const docID = targetParent.id;
+      const docID = targetParent.dataset.id;
       votePosts(docID);
+    } else if (targetParent.classList.contains("openModalDelete")) {
+      modalDelete.classList.remove("hide");
+      confirmButton.addEventListener("click", () => {
+        const docID = targetParent.dataset.id;
+        deletePost(docID);
+        modalDelete.classList.add("hide");
+      });
+    } else if (targetParent.classList.contains("openModalEdit")) {
+      modalEdit.modal.classList.remove("hide");
+      const docID = targetParent.dataset.id;
+      const postContent = targetParent.dataset.content;
+      modalEdit.inputModal.value = postContent;
+      modalEdit.confirmEditButton.addEventListener("click", () =>{
+        editPost(docID, modalEdit.inputModal.value);
+        modalEdit.modal.classList.add("hide");
+      });
     }
   });
 
